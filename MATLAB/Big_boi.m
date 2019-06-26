adea7cd2bd31a13dffc144906dd989e3ad37dfdd
Conor <cgreen18@lion.lmu.edu>
2019-06-26 14:07:52 -0500

    Tested with test data

--- MATLAB/Big_boi.m
+++ MATLAB/Big_boi.m
@@ -1,29 +1,37 @@
-
-%state variables
 clear; clc; close all; 
-%estimate scalar random constant voltage reading from a source
-figure
-set(gcf, 'color', 'k')
-kmax = 10; 
+%state variables
 
-
-nav_FileName = 'pos_and_eul_data.txt'; 
-nav_data_raw = readtable(nav_FileName, 'Delimiter',':','ReadVariableNames',false);
-nav_times = nav_data_raw{:,1};
+%kmax = 10; 
+%nav_FileName = 'pos_and_eul_data.txt'; 
+%nav_data_raw = readtable(nav_FileName, 'Delimiter',':','ReadVariableNames',false);
+%nav_times = nav_data_raw{:,1};
 %indirectly measured values
 %z = [0.39 0.50 0.48 0.29 0.25 0.32 0.34 0.48 0.41 0.45 0.39 0.50 0.48 0.29 0.25 0.32 0.34 0.48 0.41 0.45 0.39 0.50 0.48 0.29 0.25 0.32 0.34 0.48 0.41 0.45 0.39 0.50 0.48 0.29 0.25 0.32 0.34 0.48 0.41 0.45]; 
 % % % rng('shuffle','twister')
 % % % z = (.5-.39).*rand(1000,1) + 0.39; 
 % % % z = nav_data_raw{:,2:6};
-z = zeros(300,12);
-z(:,4) = 5; 
-z(:,1) = linspace(0,7.5,300);
-%intital error covariance
+
+global num_data_pts 
+num_data_pts = 300
+
+global num_sens
+num_sens = 12
+global num_states
+num_states = 12
 
 global delta_t 
 delta_t = 1/200;
-n = 12; % state size
-m = size(z,2); % observation size
+
+z = zeros(num_data_pts,num_sens);
+
+%Test at Vx=5
+z(:,4) = 5; 
+z(:,1) = linspace(0,5*delta_t*num_data_pts,num_data_pts);
+%intital error covariance
+
+
+n = num_states; % state size
+m = num_sens; % observation size
 % % % % F = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
 % % % % F = eye(n)
 R_1 = @(psi)[cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1];
@@ -51,25 +59,29 @@ F= [1,0,0,delta_t,0,0,0,0,0,0,0,0;
 
 % % % % H = [1 0 0 0; 0 1 0 0];
 H = eye(m);
-Q = 0.1*eye(n);
+Q = 0.6*eye(n);
 % R = 1*eye(m);
-R = 0.3*eye(m); % measurement noise covariance
-P0 = 10*eye(n); % variance 
+R = 0.6*eye(m); % measurement noise covariance
+P0 = .3*eye(n); % variance 
 %%%%%%%%%%%%%%%%Sensors we want%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % initx = [x, y, z ,v_x, v_y, v_z, roll, pitch  yaw, omega_x, omega_y, omega_z]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-initx = zeros(12,1); %initial state
+initx = zeros(num_states,1); %initial state
+initx(4) = z(1,:)'
 % [xfilt, Vfilt] = KalmanFilter(z, F, H, Q, R, initx, initV);
 x_estimate = KalmanFilter(z, F, H, Q, R, initx, P0);
 %grabbing the data
 estimate_data = zeros(length(x_estimate),n); 
+
 for ii = 1:length(x_estimate)
     estimate_data(ii,:) = x_estimate{ii}(:)'; 
 end 
 
 %%%%%%%%%%%% Plotting
 
+figure
+set(gcf, 'color', 'k')
 plot(1:length(x_estimate), z(:,1),'Color', 'r');hold on
 plot(1:length(x_estimate), estimate_data(:,1),'Color', 'b') 
 legend({'Measured','Kalman Estimate'},'Location','Southeast')
@@ -77,10 +89,6 @@ set(gca,'color','k')
 set(gca,'ycolor','r')
 set(gca,'xcolor','r')
 
-%%
-size(z,1);
-length(z);
-eye(size(H))
 %%
 function state = KalmanFilter(z, F, H, Q, R, X0, P0)
 R_1 = @(psi)[cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1];
