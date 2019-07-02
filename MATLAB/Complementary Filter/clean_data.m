66981198770b7c2ab91580838cb553a20371fc1c
Conor <cgreen18@lion.lmu.edu>
2019-07-02 12:34:54 -0500

    updated

--- MATLAB/Complementary Filter/clean_data.m
+++ MATLAB/Complementary Filter/clean_data.m
@@ -6,6 +6,25 @@ function [attitude , yaw_initial , altitude , velocity , magneto , mag_initial ,
 %   alt(vision)[12] : alt(raw)[13] : Ax[14] : Ay[15] : Az[16] ...
 %   Wx[17] : Wy[18] : Wz[19]
 
+t = 1
+pitch = 2
+roll = 3
+yaw = 4
+alt = 5
+vx = 6
+vy = 7
+vz = 8
+mx = 9
+my = 10
+mz = 11
+ax = 14
+ay = 15
+az = 16
+wx = 17
+wy = 28
+wz = 19
+
+
 %% Setting biases
 % Constants found during calibration rounded to 3 decimal places
 % Tested on ar drone 174...
@@ -22,16 +41,15 @@ end
 % pitch & roll predetermined.
 % yaw must be found in calibration 1:600 pts
 % yaw is erratic but luckily relatively arbitrary
-att_bias = [ -0.079 , 0.016 , 0];
+% psi , theta , phi
+att_bias = [ 0 , -0.079 , 0.016 ];
 
-yaw_data = nav_data(1:600 , 4);
+yaw_data = nav_data(1:600 , yaw);
 yaw_data_no_neg = yaw_data.*(yaw_data >= 0) + (360+yaw_data).*(yaw_data < 0);
 yaw_initial = mean(yaw_data_no_neg);
 yaw_initial = special_mod_yaw(yaw_initial);
-att_bias(3) = yaw_initial;
-% theta , phi , psi -> psi , theta , phi
-temp = att_bias;
-att_bias = [temp(3) , temp(1) , temp(2)];
+att_bias(1) = yaw_initial;
+
 
 % Vx Vy Vz
 vel_bias = [13.514 , 0.625 ,0];
@@ -40,33 +58,64 @@ vel_bias = [13.514 , 0.625 ,0];
 % Dynamic, depends on initial calibration
 % Assume first 600 points (3 sec) is sufficient considering it is very
 % gaussian
-magnetos = nav_data(1:600 , 9:11);
+magnetos = nav_data(1:600 , mx:mz);
 mag_means = mean(magnetos);
 
 % Ax Ay Az
 % After converting from LSB into G and removing gravity (Az=1), determined mean
-acc_bias = [0.095 , -0.032 , 0.007];
+acc_bias = [0.0949 , -0.0324, 0.007];
 
 % Wx Wy Wz
 gyro_bias = [28.763 , 60.581 , -14.401];
 
 
 %% Create unique variables
-temp = nav_data(: , 2:4);
+% t = 1
+% pitch = 2
+% roll = 3
+% yaw = 4
+% alt = 5
+% vx = 6
+% vy = 7
+% vz = 8
+% mx = 9
+% my = 10
+% mz = 11
+% ax = 14
+% ay = 15
+% az = 16
+% wx = 17
+% wy = 28
+% wz = 19
+
+
+temp = nav_data(: , pitch:yaw);
 attitude = [temp(:,3) , temp(:,1) , temp(:,2)];
-altitude = nav_data(:,5);
-velocity = nav_data(: , 6:8);
-magneto = nav_data(: , 9:10);
-accel_no_grav = (nav_data(:, 14:16) - 2048) / 512;
-accel_normalized = (nav_data( : , 14:16) - 2048) / 512;
-ang_velocity = nav_data(:, 17:19);
+altitude = nav_data(:,alt);
+velocity = nav_data(: , vx:vz);
+magneto = nav_data(: , mx:my);
+accel_no_grav = (nav_data(:, ax:az) - 2048) / 512;
+accel_normalized = (nav_data( : , ax:az) - 2048) / 512;
+ang_velocity = nav_data(:, wx:wz);
 
 %% Subtracting bias & Converting to nice coordinate system
-% total_mat format:
-% time[1] : pitch[2] : roll[3] : yaw[4] : alt(demo)[5] : ...
-%   Vx[6] : Vy[7] :Vz[8] : Mx[9] : My[10] : Mz[11] : ...
-%   alt(vision)[12] : alt(raw)[13] : Ax[14] : Ay[15] : Az[16] ...
-%   Wx[17] : Wy[18] : Wz[19]
+% t = 1
+% pitch = 2
+% roll = 3
+% yaw = 4
+% alt = 5
+% vx = 6
+% vy = 7
+% vz = 8
+% mx = 9
+% my = 10
+% mz = 11
+% ax = 14
+% ay = 15
+% az = 16
+% wx = 17
+% wy = 28
+% wz = 19
 
 
 % psi , theta , phi
@@ -95,7 +144,7 @@ accel_normalized = normr(accel_normalized);
 % adjust orientation to fit yaw/pitch/roll as given by API
 ang_velocity = ang_velocity - gyro_bias;
 ang_velocity(:,2) = -ang_velocity(:,2);
-ang_velocity(:,3) = -ang_velocity(:,3)
+ang_velocity(:,3) = -ang_velocity(:,3);
 
 %% Convert units
 % cm -> m
