664402d67ae07134823cd9744ec2b056dfa57c64
Brenden Stevens <brenden728@ucla.edu>
2019-07-10 11:36:58 -0500

    need this now too

---
+++ MATLAB/Cloud_stitching/grab_pico_data.m
@@ -0,0 +1,25 @@
+function [pt_cloud_data,N_Frames,pico_times] = grab_pico_data(filename)
+    disp('Gathering Data')
+    manager = royale.CameraManager();
+    cameraDevice = manager.createCamera(filename);
+    delete(manager);
+    cameraDevice.initialize();
+    % configure playback
+    % cameraDevice.loop(false);
+     cameraDevice.useTimestamps(true);
+    N_Frames=cameraDevice.frameCount();
+    % start capture mode
+    cameraDevice.startCapture();
+    
+    pt_cloud_data = cell(1,N_Frames); 
+    pico_times = zeros(N_Frames,2);
+    
+    for ii = 1:N_Frames
+        % retrieve data from camera
+        pt_cloud_data{ii} = cameraDevice.getData();
+        % retrieve times
+        pico_times(ii,1) = pt_cloud_data{ii}.timeStamp/(10^6); 
+    end 
+    
+    
+end 
